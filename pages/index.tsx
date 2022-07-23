7b3e74e19790549ead202e472572c5ddba7593b6
Jonas Flach <joansqf@gmail.com>
2022-07-23 00:35:51 -0300

    index cleaning

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,21 +5,12 @@ export default function Home() {
 	return (
 		<div className={styles.container}>
 			<Head>
-				<title>Create Next App</title>
+				<title>Sell to Go!</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
 			<main className={styles.main}>
-				<h1 className={styles.title}>
-					Welcome to <a href="https://nextjs.org">
-						Next.js!</a> integrated with{" "}
-					<a href="https://mui.com/">Material-UI!</a>
-				</h1>
-				<p className={styles.description}>
-					Get started by editing{" "}
-					<code className={styles.code}>
-						pages/index.js</code>
-				</p>
+				
 
 			</main>
 		</div>
